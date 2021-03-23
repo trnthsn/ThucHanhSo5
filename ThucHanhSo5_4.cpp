6a62cdd3a289c9f9c765b76b4bce2f746c145d80
Sown <saophaisoan23112002@gmail.com>
2021-03-23 21:06:29 +0700

    passed

--- ThucHanhSo5_4.cpp
+++ ThucHanhSo5_4.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
-
-
-
-
-int main
+void gen4(string s, int len, string s1, int i)
 {
-	return 0;
+
+    string s2 = "";
+    for(int j = 0; j < len; j ++)
+    {
+        char x = s[j];
+
+        s2 = s1 + x;
+        if(s2.length() == i)
+        {
+            cout << s2 << endl;
+        }
+        else
+        {
+            gen4(s, len, s2, i);
+        }
+    }
+}
+
+int main()
+{
+
+    string s;
+    cin >> s;
+    int len = s.length();
+    string s1 = "";
+    for(int i = 1; i <= len; i++)
+    {
+        gen4(s, len, "", i);
+    }
+    return 0;
 }
